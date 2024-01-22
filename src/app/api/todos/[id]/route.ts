7192cda434db441826bc45c9e501e9cc86aa0dba
“ariel.duarte” <arielpy@gmail.com>
2024-01-21 23:50:58 -0300

    Added Update todo mechanism

--- src/app/api/todos/[id]/route.ts
+++ src/app/api/todos/[id]/route.ts
@@ -1,5 +1,6 @@
 import prisma from "@/lib/prisma";
 import { NextResponse, NextRequest } from "next/server";
+import * as yup from "yup";
 
 interface Args {
   params: {
@@ -19,4 +20,36 @@ export async function GET(request: Request, args: Args) {
   }
 
   return NextResponse.json(todo);
+}
+
+const postSchema = yup.object({
+  description: yup.string().required(),
+  complete: yup.boolean().optional().default(false),
+});
+
+export async function PUT(request: Request, args: Args) {
+  try {
+    const { id } = args.params;
+    const { description, complete } = await postSchema.validate(
+      await request.json()
+    );
+    const todo = await prisma.todo.findFirst({
+      where: {
+        id: id,
+      },
+    });
+    if (!todo) {
+      return NextResponse.json({ message: "Todo not found" }, { status: 404 });
+    } else {
+      await prisma.todo.update({
+        where: {
+          id,
+        },
+        data: { description, complete },
+      });
+      return NextResponse.json(todo);
+    }
+  } catch (error) {
+    return NextResponse.json({ error }, { status: 400 });
+  }
 }
--- src/app/api/todos/route.ts
+++ src/app/api/todos/route.ts
@@ -1,5 +1,6 @@
 import prisma from "@/lib/prisma";
 import { NextResponse, NextRequest } from "next/server";
+import * as yup from "yup";
 
 export async function GET(request: Request) {
   const searchParam = new URL(request.url).searchParams;
@@ -17,4 +18,23 @@ export async function GET(request: Request) {
   });
 
   return NextResponse.json(todos);
+}
+
+const postSchema = yup.object({
+  description: yup.string().required(),
+  complete: yup.boolean().optional().default(false),
+});
+
+export async function POST(request: Request) {
+  try {
+    const { description, complete } = await postSchema.validate(
+      await request.json()
+    );
+    const todo = await prisma.todo.create({
+      data: { description, complete },
+    });
+    return NextResponse.json(todo, { status: 201 });
+  } catch (error) {
+    return NextResponse.json({ error }, { status: 400 });
+  }
 }
