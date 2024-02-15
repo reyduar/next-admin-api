70499e211cc22822779d5013918377c600a4f35b
“ariel.duarte” <arielpy@gmail.com>
2024-02-15 12:01:10 -0300

    Added useOptimisticHook and added revalidate data of pages todos

--- src/todos/components/TodoItem.tsx
+++ src/todos/components/TodoItem.tsx
@@ -1,5 +1,5 @@
 "use client";
-
+import { startTransition, useOptimistic } from "react";
 import { Todo } from "@prisma/client";
 import styles from "./TodoItem.module.css";
 import { IoCheckboxOutline, IoSquareOutline } from "react-icons/io5";
@@ -9,21 +9,42 @@ interface Props {
   toggleTodo: (id: string, complete: boolean) => Promise<Todo | void>;
 }
 export const TodoItem = ({ todo, toggleTodo }: Props) => {
+  const [optimisticTodo, setOptimisticTodo] = useOptimistic(
+    todo,
+    (state, newCompleteValue: boolean) => ({
+      ...state,
+      complete: newCompleteValue,
+    })
+  );
+
+  const onToggleTodo = async () => {
+    try {
+      startTransition(() => setOptimisticTodo(!optimisticTodo.complete));
+      await toggleTodo(optimisticTodo.id, !optimisticTodo.complete);
+    } catch (error) {
+      startTransition(() => setOptimisticTodo(!optimisticTodo.complete));
+    }
+  };
   return (
-    <div className={todo.complete ? styles.todoDone : styles.todoPending}>
+    <div
+      className={optimisticTodo.complete ? styles.todoDone : styles.todoPending}
+    >
       <div
-        onClick={() => toggleTodo(todo.id, !todo.complete)}
+        // onClick={() => toggleTodo(todo.id, !optimisticTodo.complete)}
+        onClick={onToggleTodo}
         className={`flex p-2 rounded-md cursor-pointer hover:bg-opacity-60 ${
-          todo.complete ? "bg-blue-100" : "bg-red-100"
+          optimisticTodo.complete ? "bg-blue-100" : "bg-red-100"
         }`}
       >
-        {todo.complete ? (
+        {optimisticTodo.complete ? (
           <IoCheckboxOutline size={30} />
         ) : (
           <IoSquareOutline size={30} />
         )}
       </div>
-      <div className="text-center sm:text-left">{todo.description}</div>
+      <div className="text-center sm:text-left">
+        {optimisticTodo.description}
+      </div>
     </div>
   );
 };
